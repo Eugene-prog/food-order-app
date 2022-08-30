e0fb1dd4ab93c317dd643bf350b6801915550a71
Eugene-prog <ieugene.prog@gmail.com>
2022-08-30 21:14:04 +0700

    Header - the first component is done

---
+++ src/Components/Layout/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+import sushiImage from "../../assets/sushi.jpg";
+import styles from "./Header.module.css";
+
+const Header = (props) => {
+  return (
+    <React.Fragment>
+      <header className={styles.header}>
+        <h1>Японская кухня</h1>
+        <button>Корзина</button>
+      </header>
+      <div className={styles["main-image"]}>
+        <img src={sushiImage} alt="Блюда японской кухни" />
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Header;
