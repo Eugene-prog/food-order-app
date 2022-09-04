2f8d67a6e8dee1fc83433614206d57ae9d2df7ac
Eugene-prog <ieugene.prog@gmail.com>
2022-09-04 16:40:39 +0700

    add components Input, MealItemForm

---
+++ src/Components/Meals/MealItem/MealItemForm.js
@@ -0,0 +1,13 @@
+import styles from "./MealItemForm.module.css";
+import Input from "../../UI/Input";
+
+const MealItemForm = (props) => {
+  return (
+    <form className={styles.form}>
+      <Input label="Количество" input={{ id: "amount", type: "number", min: "1", step: "1", defaultValue: "1" }} />
+      <button>Добавить</button>
+    </form>
+  );
+};
+
+export default MealItemForm;
