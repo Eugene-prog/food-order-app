5a3a9da19032187da5cf92dea606427140a2ff1f
Eugene-prog <ieugene.prog@gmail.com>
2022-09-04 22:54:54 +0700

    add Cart component

---
+++ src/Components/Cart/Cart.js
@@ -0,0 +1,27 @@
+import styles from "/Cart.module.css";
+
+const Cart = (props) => {
+  const cartItems = (
+    <ul className={styles["cart-items"]}>
+      {[{ id: "m1", name: "Sushi", amount: 2, price: 10.99 }].map((item) => (
+        <li>{item.name}</li>
+      ))}
+    </ul>
+  );
+
+  return (
+    <div>
+      {cartItems}
+      <div className={styles.total}>
+        <span>Итого</span>
+        <span>49.99</span>
+      </div>
+      <div className={styles.actions}>
+        <button className={styles["button--alt"]}>Закрыть</button>
+        <button className={styles["button--alt"]}>Заказать</button>
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
