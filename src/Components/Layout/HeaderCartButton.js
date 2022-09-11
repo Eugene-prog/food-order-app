d3ddd4318965389a733964afa8a48374392436bb
Eugene-prog <ieugene.prog@gmail.com>
2022-09-11 22:53:24 +0700

    add animation to basket button

--- src/Components/Layout/HeaderCartButton.js
+++ src/Components/Layout/HeaderCartButton.js
@@ -1,17 +1,35 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartContext from "../../store/cart-context";
 import CartIcon from "../Cart/CartIcon";
 import styles from "./HeaderCartButton.module.css";
 
 const HeaderCardButton = (props) => {
+  const [isButtonAnimated, setIsButtonAnimated] = useState(false);
   const cartContext = useContext(CartContext);
 
   const cartItemsNumber = cartContext.items.reduce((cur, item) => {
     return cur + item.amount;
   }, 0);
 
+  const buttonClasses = `${styles.button} ${isButtonAnimated ? styles.bump : ""}`;
+
+  useEffect(() => {
+    if (cartContext.items.length === 0) {
+      return;
+    }
+    setIsButtonAnimated(true);
+
+    const timer = setTimeout(() => {
+      setIsButtonAnimated(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [cartContext.items]);
+
   return (
-    <button className={styles.button} onClick={props.onClick}>
+    <button className={buttonClasses} onClick={props.onClick}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
